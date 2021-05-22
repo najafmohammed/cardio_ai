738350ca5f90fe2dca91f918ec631d97475bd5ae
NajafMohammed <najafmohammed17@gmail.com>
2021-05-22 07:49:45 +0530

    AI MVP

--- lib/homePages/assistant.dart
+++ lib/homePages/assistant.dart
@@ -1,3 +1,6 @@
+import 'package:cardio_ai/Core/Prediction.dart';
+import 'package:cardio_ai/shared/ColorApp.dart';
+import 'package:countup/countup.dart';
 import 'package:flutter/material.dart';
 
 class Assistant extends StatefulWidget {
@@ -8,10 +11,35 @@ class Assistant extends StatefulWidget {
 }
 
 class _AssistantState extends State<Assistant> {
+  var prediction=0.0;
+  List<double> test =[ 57,0,0,140,241,0,1,123,1,0.2,1,0,3];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('assistant',style: TextStyle(color: Colors.white),),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+       children: [
+         Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: Text(test.toString(),style: whitePopSmall,),
+         ),
+         Countup(
+           begin: (0),
+           end: prediction,
+           style: whitePopLarge(Colors.white),
+           duration: Duration(seconds: 2),
+         ),
+         ElevatedButton(onPressed: (){
+
+           var a =score(test);
+           setState(() {
+             prediction=(a[0]*100).toDouble();
+           });
+           print(a[0]);
+         }, child: Text("predict"))
+       ],
+
+      ),
     );
   }
 }
